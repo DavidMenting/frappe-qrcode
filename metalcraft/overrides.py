479763312c2d0505dadc040340cb15b11ba005f9
David Menting <david@davidmenting.nl>
2024-04-09 15:14:21 +0200

    remove metalcraft directory

--- metalcraft/overrides.py
+++
@@ -1,8 +0,0 @@
-from six import string_types
-
-# deprecated - hoxfix added
-# def _make_naming_series_key(prefix):
-# 	if not isinstance(prefix, string_types):
-# 		return ''
-# 	else:
-# 		return prefix.upper() + '.#####'
--- metalcraft/public/js/fullsize.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	let sheet = document.createElement('style');
-	sheet.setAttribute('id','fullsizestyle');
-	sheet.innerHTML = ".container {width: 90% !important;}";
-	document.body.appendChild(sheet);
-});
