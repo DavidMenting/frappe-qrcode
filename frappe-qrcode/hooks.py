c2ed2a3acf8b891dd1b30c9eda5ef4d62b58f60b
David Menting <david@davidmenting.nl>
2024-04-09 15:12:48 +0200

    Strip down to only QRcode filter

---
+++ frappe-qrcode/hooks.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from . import __version__ as app_version
+
+app_name = "frappe-qrcode"
+app_title = "Frappe QRcode"
+app_publisher = ""
+app_description = ""
+app_icon = "fa fa-fire-extinguisher"
+app_color = "red"
+app_email = ""
+app_license = "MIT"
+
+# Jinja Filters
+# ---------------
+# Methods accessible to print template
+jenv = {
+    "methods": [
+    'get_qrcode:frappe-qrcode.jinja_filters.get_qrcode'
+    ]
+}
